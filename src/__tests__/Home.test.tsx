dcf0963a5072da498d56ebf7ee4d2e81e2c96ee4
ichihatsu1 <ichihatsu1@gmail.com>
2022-06-07 12:17:31 +0700

    Edit Home Page Testing

--- src/__tests__/Home.test.tsx
+++ src/__tests__/Home.test.tsx
@@ -1,20 +1,27 @@
-import { render, screen } from "@testing-library/react";
-import { BrowserRouter } from "react-router-dom";
 import HomePage from "../pages/HomePage";
-
+import { render, screen } from "@testing-library/react";
 
 describe('Home Page Test', () => {
-    const page: JSX.Element = <BrowserRouter><HomePage /></BrowserRouter>
-    it('Should have Header Text', () => {
+    // const page: JSX.Element = <BrowserRouter><HomePage /></BrowserRouter>
+    const page: JSX.Element = <HomePage />
+
+    it('Should have React Image', () => {
         render(page);
-        const elemet = screen.getByText(/Sewa & Rental Mobil Terbaik di kawasan \(Lokasimu\)/i)
+        const elemet = screen.getByAltText(/logo/i)
         expect(elemet).toBeInTheDocument();
         expect(elemet).toBeVisible();
     })
 
-    it('Should have Car Image', () => {
+    it('Should have Header Text', () => {
         render(page);
-        const elemet = screen.getByAltText(/Car/i)
+        const elemet = screen.getByText(/Challenge 08/i)
+        expect(elemet).toBeInTheDocument();
+        expect(elemet).toBeVisible();
+    })
+
+    it('Should have Subtitle Text', () => {
+        render(page);
+        const elemet = screen.getByText(/Implementing CI\/CD Flows with Github Actions/i)
         expect(elemet).toBeInTheDocument();
         expect(elemet).toBeVisible();
     })
