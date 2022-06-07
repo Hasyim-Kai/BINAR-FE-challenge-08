bd6bc461f4ae13e4f7750bbfc238a85f2f907313
ichihatsu1 <ichihatsu1@gmail.com>
2022-06-07 20:18:27 +0700

    Edit Homepage version to 0.7

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,7 +2,7 @@ export default function HomePage() {
     return <main className='bg-gradient-to-br from-gray-700 via-cyan-900 to-gray-900 w-full min-h-screen flex flex-col justify-center items-center text-white'>
         <img className="w-64" src="/logo512.png" alt="Logo" />
         <h1 className="text-6xl font-semibold mb-2">Challenge 08</h1>
-        <p className="text-4xl mb-4">Version 0.0</p>
+        <p className="text-4xl mb-4">Version 0.7</p>
         <p className="text-gray-400">Implementing CI/CD Flows with Github Actions</p>
     </main>
 }
