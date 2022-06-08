12a1854cc86e180ec4e73eba462d778e5bee7661
Muhammad Hasyim Chaidir Ali <ichihatsu1@gmail.com>
2022-06-08 08:40:21 +0700

    Update Homepage version to 1.0

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,8 +1,8 @@
 export default function HomePage() {
-    return <main className='bg-gradient-to-br from-gray-700 via-cyan-900 to-gray-900 w-full min-h-screen flex flex-col justify-center items-center text-white'>
-        <img className="w-64" src="/logo512.png" alt="Logo" />
-        <h1 className="text-6xl font-semibold mb-2">Challenge 08</h1>
-        <p className="text-4xl mb-4">Version 0.7</p>
-        <p className="text-gray-400">Implementing CI/CD Flows with Github Actions</p>
-    </main>
+  return <main className='bg-gradient-to-br from-gray-700 via-cyan-900 to-gray-900 w-full min-h-screen flex flex-col justify-center items-center text-white'>
+    <img className="w-64" src="/logo512.png" alt="Logo" />
+    <h1 className="text-6xl font-semibold mb-2">Challenge 08</h1>
+    <p className="text-4xl mb-4">Version 1.0</p>
+    <p className="text-gray-400">Implementing CI/CD Flows with Github Actions</p>
+  </main>
 }
